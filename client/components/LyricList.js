e0336f9f87a80a7959c4f309ddb1ffc33043e96a
robinHurtado <robin_aven94@hotmail.com>
2018-04-26 22:16:07 -0500

    Add thumb up icon, add likes field in the mutation of lyricCreate.js

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
   constructor(props) {
     super(props);
   }
 
+  onLike(id){
+    this.props.mutate({ variables: { id } });
+  }
+
   render() {
     return (
       <ul className="collection">
         {
-          this.props.lyrics.map(({ id, content}) => (
-            <li key={id} className="collection-item">{content}</li>
+          this.props.lyrics.map(({ id, content, likes }) => (
+            <li key={id} className="collection-item">
+              {content}
+              <div className="vote-box">
+                <i
+                  className="material-icons"
+                  onClick={() => this.onLike(id)}
+                >
+                  thumb_up
+                </i>
+                {likes}
+              </div>
+            </li>
           ))
         }
       </ul>
@@ -18,4 +35,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID){
+    likeLyric(id: $id){
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
